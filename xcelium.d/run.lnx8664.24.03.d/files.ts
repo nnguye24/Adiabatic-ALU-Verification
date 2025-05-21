b6dbc685c6480d0bb9b51d8fb61d6686496d576b
Nguyen Nguyen <nnguye24@node03.cse.nd.edu>
2025-05-21 18:43:12 -0400

    xrun configured

---
+++ xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -0,0 +1,3 @@
+1709675779 /afs/crc.nd.edu/group/cse/cadence2/installs/XCELIUM2403/tools.lnx86/methodology/UVM/CDNS-IEEE/sv/src/uvm_macros.svh
+1747866810 /afs/crc.nd.edu/user/g/gsnider/MIPS25/V1.10.513/VirtuosoOA/examples/Adiabatic-ALU-Verification/adder_consolidated.sv
+1747866827 /afs/crc.nd.edu/user/g/gsnider/MIPS25/V1.10.513/VirtuosoOA/examples/Adiabatic-ALU-Verification/testbench_consolidated.sv
